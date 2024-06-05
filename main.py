cc9de4df0e4188234ff889e34baba23fef2ec172
Mert <62549656+fswair@users.noreply.github.com>
2024-06-05 18:44:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -150,8 +150,7 @@ async def fetch_lyrics(lid: str, request: Request, data: schemas.Credentials = N
 @app.get("/get_credits")
 async def fetch_credits(request: Request, data: schemas.Credentials = None):
     credentials = dict(request.session) or data.dict() if data else {}
-    print(credentials)
-    if credentials.get("cookie") is None:
+    if not credentials:
         return {"error": "Credentials not found in cookie. Please setup or pass credentials as json data."}
     else:
         try:
